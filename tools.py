8bb4e7ba5e5957457bc47d9339aef45568eb1684
Rayya-Haqqani <rayya.haqqani@gmail.com>
2024-06-16 17:31:06 +0500

    Add files via upload

---
+++ tools.py
@@ -0,0 +1,38 @@
+import os
+from exa_py import Exa
+from langchain.agents import tool
+
+class ExaSearchToolSet():
+    @tool
+    def search(query: str):
+        """Search for a webpage based on the query."""
+        return ExaSearchToolSet._exa().search(f'{query}', use_autoprompt=True, num_results=3)
+
+    @tool
+    def find_similar(url: str):
+        """Search for webpages similar to a given URL.
+        The url passed in should be a URL returned from 'search'.
+        """
+        return ExaSearchToolSet._exa().find_similar(f'{url}', num_results=3)
+
+    @tool
+    def get_contents(ids: str):
+        """Get the content of a webpage.
+        The ids must be passed in as a list, a list of ids returned from 'search'.
+        """
+        ids = eval(ids)
+
+        contents = str[ExaSearchToolSet._exa().get_contents(ids)]
+        contents = contents.split("URL:")
+        contents = [contents[:1000] for content in contents]
+        return "\n\n".join(contents)
+
+    def tools():
+        return [
+            ExaSearchToolSet.search,
+            ExaSearchToolSet.find_similar,
+            ExaSearchToolSet.get_contents
+        ]
+
+    def _exa():
+        return Exa(api_key=os.environ.get("EXA_API_KEY"))
